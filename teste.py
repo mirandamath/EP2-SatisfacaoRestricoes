29987e4e4802d0bc7ebbbc13c3080a720ad7d3ff
Matheus Miranda <lifematheusmiranda@gmail.com>
2023-03-18 20:57:38 -0300

    teste

--- teste.py
+++ teste.py
@@ -1,89 +1,45 @@
 import numpy as np
 from satisfacao_restricoes import Restricao, SatisfacaoRestricoes
 
-# Definir as variáveis e seus respectivos domínios
-equipamentos = ['Balança Analítica', 'Agitador Magnético', 'Cromatógrafo Líquido', 
-                'Cromatógrafo Gasoso', 'Espectrofotômetro UV-VIS', 'Espectrômetro Infravermelho', 
-                'Espectrômetro de Massa', 'Microscópio']
 
-analises = ['Análise 1', 'Análise 2', 'Análise 3', 'Análise 4', 'Análise 5', 
-            'Análise 6', 'Análise 7', 'Análise 8', 'Análise 9', 'Análise 10']
 
-dominios = {}
-for equipamento in equipamentos:
-    dominios[equipamento] = analises
 
-# Definir as restrições
-class UsoDiarioMaximo(Restricao):
-    def __init__(self, equipamento, tempo_maximo):
-        super().__init__([equipamento])
-        self.equipamento = equipamento
-        self.tempo_maximo = tempo_maximo
+if __name__ == "__main__":
 
-    def esta_satisfeita(self, atribuicao):
-        if self.equipamento not in atribuicao:
-            return True
+    variaveis = []
 
-        tempo_uso = sum([atribuicao[var] == self.equipamento for var in atribuicao])
-        return tempo_uso <= self.tempo_maximo
+    equipamentos = []
 
-class AnalisesConcorrentes(Restricao):
-    def __init__(self, equipamento_1, equipamento_2):
-        super().__init__([equipamento_1, equipamento_2])
-        self.equipamento_1 = equipamento_1
-        self.equipamento_2 = equipamento_2
-
-    def esta_satisfeita(self, atribuicao):
-        if self.equipamento_1 not in atribuicao or self.equipamento_2 not in atribuicao:
-            return True
-
-        return atribuicao[self.equipamento_1] != atribuicao[self.equipamento_2]
-
-class DuracaoAnalise(Restricao):
-    def __init__(self, equipamento, max_duracao):
-        super().__init__([equipamento])
-        self.equipamento = equipamento
-        self.max_duracao = max_duracao
+    analises = {"Analise1" : ["Espectrofotômetro UV-VIS", "Cromatógrafo Gasoso"],
+                "Analise2" : ["Cromatógrafo Líquido" , "Espectrômetro Infravermelho"],
+                "Analise3" : ["Microscópio", "Balança Analítica"],
+                "Analise4" : ["Espectrômetro de Massa"],
+                "Analise5" : ["Agitador Magnético", "Espectrômetro Infravermelho"],
+                "Analise6" : ["Cromatógrafo Líquido" , "Espectrofotômetro UV-VIS"],
+                "Analise7" : ["Espectrofotômetro UV-VIS", "Microscópio"],
+                "Analise8" : ["Cromatógrafo Gasoso"],
+                "Analise9" : ["Espectrômetro Infravermelho", "Balança Analítica"],
+                "Analise10" : ["Espectrômetro de Massa" , "Cromatógrafo Gasoso"]                
+                }
     
-    def esta_satisfeita(self, atribuicao):
-        dominio_equipamento = atribuicao[self.equipamento]
-        duracao_analise = 0
+    for analise, valor in analises.items():
+        for equipamento in valor:
+            variaveis.append(analise + "_" + equipamento)
+            if equipamento not in equipamentos:
+                equipamentos.append(equipamento)
 
-        for valor in dominio_equipamento:
-            duracao_analise = len(atribuicao)
-        return duracao_analise <= self.max_duracao
+    dominio = {}
 
-# for equipamento in equipamentos:
-#     restricoes.append(DuracaoAnalise(equipamento))
+    for variavel in variaveis:
+        dominio[variavel] = [1,2,3,4,5,6,7,8]
 
-# Resolver o problema usando busca de backtracking
-problema = SatisfacaoRestricoes(equipamentos, dominios)
+    problema = SatisfacaoRestricoes(variaveis, dominio)
 
-problema.adicionar_restricao(AnalisesConcorrentes('Balança Analítica', 'Microscópio'))
-problema.adicionar_restricao(AnalisesConcorrentes('Agitador Magnético', 'Espectrômetro Infravermelho'))
-problema.adicionar_restricao(AnalisesConcorrentes('Cromatógrafo Líquido', 'Cromatógrafo Gasoso'))
-problema.adicionar_restricao(AnalisesConcorrentes('Espectrofotômetro UV-VIS', 'Microscópio'))
+    # problema.adicionar_restricao(RestricaoEmUso("Analise1_Espectrofotômetro UV-VIS", "Espectrofotômetro UV-VIS"))
 
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[0], 6))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[1], 4))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[2], 8))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[3], 6))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[4], 4))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[5], 6))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[6], 4))
-problema.adicionar_restricao(UsoDiarioMaximo(equipamentos[7], 6))
+    solucao = problema.busca_backtracking()
 
-problema.adicionar_restricao(DuracaoAnalise(equipamentos[0], 2))
-
-solucao = problema.busca_backtracking()
-
-print(problema.variaveis)
-
-print(problema.dominios)
-
-# if solucao is not None:
-#     print("Solução encontrada:")
-#     for equipamento, sala in solucao.items():
-#         print(f"{equipamento} -> {sala}")
-# else:
-#     print("Não foi possível encontrar uma solução para o problema.")
+    print(variaveis)
+    print(equipamentos)
+    print(dominio)
+    
