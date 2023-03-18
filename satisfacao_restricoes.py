61c9cc353bc9d9ccfd7baf1d11afdc924b3ba4a1
Matheus Miranda <lifematheusmiranda@gmail.com>
2023-03-18 20:03:41 -0300

    first commit (nao tem nada funcionando ainda)

---
+++ satisfacao_restricoes.py
@@ -0,0 +1,52 @@
+class Restricao():
+    def __init__(self, variaveis):
+        self.variaveis = variaveis
+
+    def esta_satisfeita(self, atribuicao):
+      return True
+
+class SatisfacaoRestricoes():
+  def __init__(self, variaveis, dominios):
+    self.variaveis = variaveis # Variáveis para serem restringidas
+    self.dominios = dominios # Domínio de cada variável
+    self.restricoes = {}
+    for variavel in self.variaveis:
+        self.restricoes[variavel] = []
+        if variavel not in self.dominios:
+            raise LookupError("Cada variávei precisa de um domínio")
+
+  def adicionar_restricao(self, restricao):
+    for variavel in restricao.variaveis:
+      if variavel not in self.variaveis:
+        raise LookupError(f"Variável {variavel} não definida previamente")
+      else:
+        self.restricoes[variavel].append(restricao)
+
+  def esta_consistente(self, variavel, atribuicao):
+    for restricoes in self.restricoes[variavel]:
+      if not restricoes.esta_satisfeita(atribuicao):
+        return False
+    return True
+  
+  def busca_backtracking(self, atribuicao = {}):
+    # retorna sucesso quando todas as variáveis forem atribuídas
+    if len(atribuicao) == len(self.variaveis):
+      return atribuicao
+
+    # pega todas as variáveis que ainda não foram atribuídas
+    variaveis_nao_atribuidas  = [v for v in self.variaveis if v not in atribuicao]
+
+    primeira_variavel = variaveis_nao_atribuidas[0]
+    for valor in self.dominios[primeira_variavel]:
+      atribuicao[primeira_variavel] = valor
+      # estamos consistentes, seguir recursão
+      if self.esta_consistente(primeira_variavel, atribuicao):
+        atribuicao_local = atribuicao.copy()
+        atribuicao[primeira_variavel] = None
+        resultado  = self.busca_backtracking(atribuicao_local)
+        # para o backtracking se não encontra todos os resultados
+        if resultado is not None:
+          return resultado
+      else:
+        atribuicao[primeira_variavel] = None
+    return None
